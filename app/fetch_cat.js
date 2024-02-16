16f563efe7b95312ae1abec504179feda3db283f
Watanabe Tsubasa (渡邊 翼) <watanabe-tsuba@aeonpeople.biz>
2024-02-16 10:31:27 +0900

    upd

--- app/fetch_cat.js
+++ app/fetch_cat.js
@@ -1,3 +1,29 @@
-fetch('https://api.thecatapi.com/v1/images/search')
-  .then(res => res.json())
-  .then(data => console.log(data[0]))
+// fetch('https://api.thecatapi.com/v1/images/search')
+//   .then(res => res.json())
+//   .then(data => console.log(data[0]))
+
+const getCatRes = async () => {
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  })
+
+  try {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search');
+    const json = res.json();
+    resolve(json)
+    console.log('resolve!');
+  } catch (error) {
+    reject(error)
+    console.log('reject!');
+  }
+  return promise
+}
+
+const main = async () => {
+  const res = await getCatRes()
+  console.log(res);
+}
+
+main();
